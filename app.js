ec6cc10a3689a0034cd77b08a8b20cab85d4dce3
KurobaM <kuroba.m17@gmail.com>
2026-09-24 10:43:58 +0800

    Add download function

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const sidebar = document.getElementById("sidebar");
 
+const dl = document.getElementById("download");
+dl.addEventListener("click", downloadPatch);
+
 
 function toggleSubMenu(button) {
     if (!button.nextElementSibling.classList.contains("show")) {
@@ -11,4 +14,25 @@ function toggleSubMenu(button) {
 
     button.nextElementSibling.classList.toggle("show");
     button.classList.toggle("rotate");
+}
+
+async function downloadPatch(){
+    const a = document.createElement("a");
+    a.style.display = 'none';
+    a.href = "download/patch.zip";
+    const name = "Hajimari_no_Ishi";
+    a.download = name + ".zip";
+    try {
+        const info = await fetch("download/release_info.txt");
+        if (info.ok){
+            const data = await info.text();
+            const version = data.split('\n')[0].split(' ')[2];
+            a.download = name + "_v" + version + ".zip";
+        }
+    } catch (error) {
+        console.log(error);
+    }
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
 }
